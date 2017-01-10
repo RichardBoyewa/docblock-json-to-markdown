e3830c7f2730cb9d2d7339d9020930de42e574fc
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-10 17:21:18 +0100

    Up

--- dist/templates/default.js
+++ dist/templates/default.js
@@ -5,11 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = defaultTemplate;
 function defaultTemplate(data) {
-	return [this._config.headPart(data), this._config.constructorPart(data), this._config.groupPart(data.filter(function (block) {
+	return [this._config.parts.head(data), this._config.parts.constructor(data), this._config.parts.group(data.filter(function (block) {
 		return block.setting === true;
 	}), {
 		title: 'Settings',
 		description: "Here's the list of available settings that you can pass to the constructor"
-	}), this._config.propertiesPart(data), this._config.methodsPart(data)].join("\n");
+	}), this._config.parts.properties(data), this._config.parts.methods(data)].join("\n");
 }
 module.exports = exports["default"];
--- src/templates/default.js
+++ src/templates/default.js
@@ -1,14 +1,14 @@
 export default function defaultTemplate(data) {
 	return [
-		this._config.headPart(data),
-		this._config.constructorPart(data),
-		this._config.groupPart(data.filter((block) => {
+		this._config.parts.head(data),
+		this._config.parts.constructor(data),
+		this._config.parts.group(data.filter((block) => {
 			return block.setting === true;
 		}), {
 			title : 'Settings',
 			description : "Here's the list of available settings that you can pass to the constructor"
 		}),
-		this._config.propertiesPart(data),
-		this._config.methodsPart(data)
+		this._config.parts.properties(data),
+		this._config.parts.methods(data)
 	].join("\n");
 }
