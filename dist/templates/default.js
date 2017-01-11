b0e0bf58de678c7f86d31a7fb312ffc2cd5db912
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-11 08:24:06 +0100

    Add examples group that display styleguide docblocks

--- dist/templates/default.js
+++ dist/templates/default.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
@@ -6,10 +6,15 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = defaultTemplate;
 function defaultTemplate(data) {
 	return [this._config.parts.head(data), this._config.parts.constructor(data), this._config.parts.group(data.filter(function (block) {
+		return block.styleguide;
+	}), {
+		title: 'Examples',
+		description: "Here's some usage examples."
+	}), this._config.parts.group(data.filter(function (block) {
 		return block.setting === true;
 	}), {
 		title: 'Settings',
-		description: "Here's the list of available settings that you can pass to the constructor"
+		description: "Here's the list of available settings that you can pass to the constructor."
 	}), this._config.parts.properties(data), this._config.parts.methods(data)].join("\n");
 }
-module.exports = exports["default"];
+module.exports = exports['default'];
--- src/templates/default.js
+++ src/templates/default.js
@@ -3,10 +3,16 @@ export default function defaultTemplate(data) {
 		this._config.parts.head(data),
 		this._config.parts.constructor(data),
 		this._config.parts.group(data.filter((block) => {
+			return block.styleguide;
+		}), {
+			title : 'Examples',
+			description : "Here's some usage examples."
+		}),
+		this._config.parts.group(data.filter((block) => {
 			return block.setting === true;
 		}), {
 			title : 'Settings',
-			description : "Here's the list of available settings that you can pass to the constructor"
+			description : "Here's the list of available settings that you can pass to the constructor."
 		}),
 		this._config.parts.properties(data),
 		this._config.parts.methods(data)
