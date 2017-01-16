7d095f5a79c4c586c8d669d7427d8f56a93218b3
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-16 15:09:34 +0100

    Update sass template

--- dist/templates/scss.js
+++ dist/templates/scss.js
@@ -1,12 +1,20 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 exports.default = scssTemplate;
 function scssTemplate(data) {
-	return [this._config.parts.head(data), this._config.parts.constructor(data), this._config.parts.properties(data), this._config.parts.methods(data, {
-		title: 'Mixins'
+	return [this._config.parts.head(data), this._config.parts.group(data.filter(function (block) {
+		return block.mixin === true;
+	}), {
+		title: 'Mixins',
+		description: "Here's the list of available mixins."
+	}), this._config.parts.group(data.filter(function (block) {
+		return block.function === true;
+	}), {
+		title: 'Functions',
+		description: "Here's the list of available functions."
 	})].join("\n");
 }
-module.exports = exports["default"];
+module.exports = exports['default'];
--- src/templates/scss.js
+++ src/templates/scss.js
@@ -1,10 +1,17 @@
 export default function scssTemplate(data) {
 	return [
 		this._config.parts.head(data),
-		this._config.parts.constructor(data),
-		this._config.parts.properties(data),
-		this._config.parts.methods(data, {
-			title : 'Mixins'
+		this._config.parts.group(data.filter((block) => {
+			return block.mixin === true;
+		}), {
+			title : 'Mixins',
+			description : "Here's the list of available mixins."
+		}),
+		this._config.parts.group(data.filter((block) => {
+			return block.function === true;
+		}), {
+			title : 'Functions',
+			description : "Here's the list of available functions."
 		})
 	].join("\n");
 }
